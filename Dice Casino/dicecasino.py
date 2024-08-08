fcef4431debc6152fd68a8924b7d7511367229f9
7978SHIVAM <89209166+7978SHIVAM@users.noreply.github.com>
2024-08-09 03:49:28 +0530

    Update dicecasino.py

--- Dice Casino/dicecasino.py
+++ Dice Casino/dicecasino.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 from dotenv import load_dotenv
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Dice
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes, MessageHandler, filters
 import random
 
